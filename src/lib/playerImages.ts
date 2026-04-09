c602e9371104f325521c8d78b7c51de8a9436f76
NickDiGirolamo <nickdigirolamo4@gmail.com>
2026-04-08 22:00:16 -0700

    Update Anthony Davis image override

--- src/lib/playerImages.ts
+++ src/lib/playerImages.ts
@@ -69,6 +69,8 @@ const directImageOverrides: Record<string, string> = {
     "https://michigansportshof.org/mshof/wp-content/uploads/2019/01/ben-wallace.jpg",
   "chris-paul":
     "https://legacymedia.sportsplatform.io/images_root/slides/photos/000/538/714/106958476_original.jpg?1291243979",
+  "anthony-davis":
+    "https://www.usatoday.com/gcdn/presto/2019/01/25/USAT/8face4bf-fda9-4585-8fce-19d456e2fe5d-2019-01-24_Anthony_Davis1.jpg?crop=1744,2326,x365,y211",
   "shaquille-o-neal":
     "https://preview.redd.it/who-would-you-rather-have-prime-shaq-or-prime-giannis-v0-d0ogt1n4r6pe1.jpg?width=640&crop=smart&auto=webp&s=d72c0c9849e683ce07b4bfbe0ed627556c0247c5",
   "shawn-kemp":
