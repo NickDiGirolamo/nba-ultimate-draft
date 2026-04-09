4e855113765c5fdc26ccf0d212299f816d3e3535
NickDiGirolamo <nickdigirolamo4@gmail.com>
2026-04-08 21:14:31 -0700

    Add custom Shawn Marion image override

--- src/lib/playerImages.ts
+++ src/lib/playerImages.ts
@@ -49,6 +49,8 @@ const directImageOverrides: Record<string, string> = {
     "https://i.ebayimg.com/images/g/K9IAAOSwH-9emMX0/s-l1200.jpg",
   "shai-gilgeous-alexander":
     "https://cdn.prod.website-files.com/64da5279f1559b26fb07550e/6834c822e2ddaf129383c171_Best%20Shai%20Gilgeous-Alexander%20Pick%20for%20Timberwolves%20vs.%20Thunder%20Game%204.jpg",
+  "shawn-marion":
+    "https://cdn.nba.com/teams/legacy/www.nba.com/suns/sites/suns/files/shawn_marion_retires_35.jpg",
   "steph-curry":
     "https://compote.slate.com/images/24605cda-82b1-4342-9af9-4b86f684174b.jpg",
 };
