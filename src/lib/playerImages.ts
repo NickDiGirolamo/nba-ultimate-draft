1808ae776ed6bbd233b9e9ad22c69544bacb8c87
NickDiGirolamo <nickdigirolamo4@gmail.com>
2026-04-08 20:56:23 -0700

    Add custom Karl Malone image override

--- src/lib/playerImages.ts
+++ src/lib/playerImages.ts
@@ -21,6 +21,8 @@ const wikiTitleOverrides: Record<string, string> = {
 const directImageOverrides: Record<string, string> = {
   "kareem-abdul-jabbar":
     "https://www.jsonline.com/gcdn/presto/2021/04/12/PMJS/22f3502c-f3ef-4f9c-8875-bf7a5b6c9e46-1970_MJS_Milwaukee_Bucks_Kareem_Abdul-Jabbar_Historical_Archive-1.jpg?width=660&height=1036&fit=crop&format=pjpg&auto=webp",
+  "karl-malone":
+    "https://i.pinimg.com/736x/73/d9/68/73d968077baa340b28ac64d2e9054b84.jpg",
   "kobe-bryant":
     "https://andscape.com/wp-content/uploads/2020/10/Kobe-Bryant-Game-5-2008-Finals-e1603379029250.jpg?w=800",
   "larry-bird":
